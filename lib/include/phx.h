b3a5c87937e5aed58cbad124675d3c7924c35f65
robertking <superobertking@icloud.com>
2023-10-06 18:49:01 +0000

    Add missing extern C scope

--- lib/include/phx.h
+++ lib/include/phx.h
@@ -1,6 +1,10 @@
 #ifndef _PHX_H_
 #define _PHX_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* ====== recovery mode status API ====== */
 int phx_is_recovery_mode(void);
 void phx_finish_recovery(void);
@@ -14,5 +18,9 @@ void phx_restart_multi(void *data, void *start_arr, void *end_arr,
                     unsigned int len);
 void phx_get_preserved_multi(void **data, void **start_arr, void **end_arr,
                             unsigned int* len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _PHX_H_ */
